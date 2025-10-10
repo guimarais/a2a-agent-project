3f0085eb5bd6b9451e136f8e9347de5f24d323a3
guimarais <luis.guimarais@gmail.com>
2025-10-11 00:09:55 +0100

    I forgot adding .env to the project...

--- src/server/agent_executor.py
+++ src/server/agent_executor.py
@@ -6,28 +6,28 @@ from a2a.server.agent_execution import AgentExecutor, RequestContext
 from a2a.server.events import EventQueue
 from a2a.utils import new_agent_text_message
 from dotenv import load_dotenv
-from openai import AsyncOpenAI
+from openai import OpenAI
 
 load_dotenv()
 
-async def good_title():
+def good_title():
     """
     Creates a fake book title
     """
 
-    client = AsyncOpenAI()
+    client = OpenAI()
 
-    response = await client.chat.completions.create(
+    response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a creative book editor"},
             {"role": "user", "content": "Create an original book title involving ghosts."}
         ],
         temperature=0.8,
-        mas_tokens=150
+        max_tokens=150
     )
 
-    await client.close()
+    client.close()
 
     return response.choices[0].message.content
 
